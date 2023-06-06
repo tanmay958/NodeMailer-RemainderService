86a448e8870e55c14409213a07e5e3d92fb19929
tanmay958 <studytime958@gmail.com>
2023-06-06 18:43:33 +0530

    added the functionality of giving boarding pass 48hr prior to flight departure

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ const express =  require("express");
 const bodyParser =  require("body-parser");
 var cron = require('node-cron');
 const {PORT} =  require("./config/server-config");
+const jobs = require('./utils/job');
+const TicketController = require('./controllers/ticket-controller');
 
 const {sendBasicEmail} =  require("./services/email-service");
 const setupServer =  ()=>{
     const app =  express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    app.post('/api/v1/tickets', TicketController.create);
     app.listen(PORT,()=>{
         console.log("server started");
-
+        jobs();
         // sendBasicEmail(
         //     'tanmay958.nitt@gmail.com',
         //     'tanmaymandal958@gmail.com',
@@ -19,9 +22,9 @@ const setupServer =  ()=>{
         // )
        
 
-cron.schedule('* * * * *', () => {
-  console.log('running a task every minute');
-});
+// cron.schedule('* * * * *', () => {
+//   console.log('running a task every minute');
+// });
         
 
     })
